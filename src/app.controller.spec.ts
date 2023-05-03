32f6ce419726a2361d0000fb03b2cfcf27b0fdf2
Andrey Ilin <neform@gmail.com>
2023-05-03 18:50:37 +0200

    fix: fixed initial tests, changed jest config

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,8 +1,10 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ForbiddenException } from "@nestjs/common";
+import { Test, TestingModule } from "@nestjs/testing";
 
-describe('AppController', () => {
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+
+describe("AppController", () => {
   let appController: AppController;
 
   beforeEach(async () => {
@@ -14,9 +16,9 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe("root", () => {
+    it("should return ForbiddenException", () => {
+      expect(() => appController.root()).toThrowError(ForbiddenException);
     });
   });
 });
